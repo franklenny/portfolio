2b73b8c9eb4592b5e886b4daa95886eb247ef744
Faye Hayes <fhayes30@gmail.com>
2017-10-14 17:14:07 -0700

    verify database.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,28 @@
 class SkillsController < ApplicationController
   def index
-  end 
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def delete
+  end
+
+  private
+
+  def skill_params
+  end
 end
