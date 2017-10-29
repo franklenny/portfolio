80bced10280a9cf4a52a242b426510ec8adff804
Faye Hayes <fhayes30@gmail.com>
2017-10-29 16:20:46 -0700

    update image. Add bootstrap to gems. Add render all projects funtionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create({
 10.times do |project|
   Project.create({
     name: "Test app",
-    image_url: "https://pixabay.com/photo-1012681/",
+    image_url: "https://tctechcrunch2011.files.wordpress.com/2015/04/codecode.jpg?w=659&zoom=2",
     description: "#{project} getting things done!",
     link_to_project: "https://github.com/fhayes301/Lab-Tracker",
     user_id: 1
