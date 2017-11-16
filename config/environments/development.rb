05468bcf95dcfc546097be2931547a0edcbed3d2
Faye Hayes <fhayes30@gmail.com>
2017-11-16 00:36:37 -0800

    Implement contact form with email capability (finally)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,20 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = true
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        authentication:       :plain,
+        user_name:            ENV['GMAIL_USERNAME'],
+        password:             ENV['GMAIL_PW'],
+        enable_starttls_auto: true,
+        :openssl_verify_mode  => 'none'
+      }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,5 +66,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  
+
 end
