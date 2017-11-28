468d68e0ad4aac3a994fdb6fd4d3ab272655dac1
Faye Hayes <fhayes30@gmail.com>
2017-11-27 23:50:04 -0800

    update simple form. working to ajaxify the form to render everything on single page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,13 +1,18 @@
 class ContactsController < ApplicationController
   def new
     @contact = Contact.new
+    if request.xhr?
+      p "Thanks!"
+    end
   end
 
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      respond_to do |format|
+        format.js { render 'contacts/update'}
+      end
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
